380d2ef54ebf945d7f7dee78e3e38cf32c487d7d
atch <alexandro.tommy@gmail.com>
2024-11-03 22:50:58 +0000

    added filter

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,75 +39,88 @@ df_gs = df_gs[df_gs.row_number==1][['description','category']]
 # Load the data from a CSV. We're caching this so it doesn't reload every time the app
 # reruns (e.g. if the user interacts with the widgets).
 @st.cache_data
-def import_pdf(text):
+def extract_content_df(text):
     table = text.split('\n')
     #print('Conta:'+ table[1])
     table[9] = table[8] + ' ' +  table[9]
 
-    transacoes = []
+    trx_arr = []
 
     for row in table[9:]:
-        saldo = re.sub(r".*(?<=\d\.\d\d\s)", "", row)
-        data = re.sub(r" (?=\d+\.\d\d).*", "", row)
-        valores = re.sub(r".*(?<!\d\d\.\d\d)\s", "", row)
-        valor = valores.split(' ')[0]
+        balance = re.sub(r".*(?<=\d\.\d\d\s)", "", row)
+        trx_date = re.sub(r" (?=\d+\.\d\d).*", "", row)
+        trx_val = re.sub(r".*(?<!\d\d\.\d\d)\s", "", row).split(' ')[0]
 
-        pattern = r"^\d+\.\d+ \d+\.\d+ (.*?) \d+\.\d+ \d+\.\d+$"
-        match = re.search(pattern, row)
+        desc_regex = r"^\d+\.\d+ \d+\.\d+ (.*?) \d+\.\d+ \d+\.\d+$"
+        match = re.search(desc_regex, row)
         if match:
-            descricao = match.group(1)
-            descricao = re.sub(r"\d\d\d\d\s", "", descricao)
-            descricao = re.sub(r"\s+", " ", descricao)
+            trx_description = match.group(1)
+            trx_description = re.sub(r"\d\d\d\d\s", "", trx_description)
+            trx_description = re.sub(r"\s+", " ", trx_description)
 
-        tipo_trans = descricao.split(' ')[0]
-        descricao = ' '.join(descricao.split(' ')[1:])
+        trx_type = trx_description.split(' ')[0]
+        trx_description = ' '.join(trx_description.split(' ')[1:])
 
         try:
-            date_trx = datetime.date(2024, int(data.split('.')[0].strip()), int(data.split('.')[1]))
+            date_trx = datetime.date(2024, int(trx_date.split('.')[0].strip()), int(trx_date.split('.')[1]))
         except:
             pass
         try:
-            trx_val = float(valor)
+            trx_val = float(trx_val)
         except:
             pass
-        trans_obj = {'month': date_trx.month,
-                     'trx_type': tipo_trans,
-                     'description': descricao,
+
+        trx_obj = {'month': date_trx.month,
+                     'trx_type': trx_type,
+                     'description': trx_description,
                      'value': trx_val
                      }
-        transacoes.append(trans_obj)
+        trx_arr.append(trx_obj)
         
-    return pd.DataFrame.from_dict(transacoes)
+    return pd.DataFrame.from_dict(trx_arr)
 
 
 uploaded_files = st.file_uploader(
     "Selecione seus extratos em PDF", accept_multiple_files=True, type=['pdf']
 )
 
-text = ""
+
+def extract_pdf_content(uploaded_file):
+    print("filename:" + uploaded_file.name)
+    # To convert to a string based IO:
+    pdfio = BytesIO(uploaded_file.getvalue())
+    reader = PdfReader(pdfio)
+    number_of_pages = len(reader.pages)
+    pdf_content = ""
+    for i in range(0, number_of_pages):
+        page = reader.pages[i]
+        pdf_content = pdf_content + page.extract_text()
+    pdf_content = re.sub(r"[\S\s]*CONTA SIMPLES N.", "", pdf_content)
+    pdf_content = re.sub(r"SALDO FINAL[\S\s]*", "", pdf_content)
+    return pdf_content
+
+
 df_trx = pd.DataFrame()
 if uploaded_files:
     for uploaded_file in uploaded_files:
         #bytes_data = uploaded_file.read()
-        print("filename:" + uploaded_file.name)
-
-        # To convert to a string based IO:
-        pdfio = BytesIO(uploaded_file.getvalue())
-
-        reader = PdfReader(pdfio)
-        number_of_pages = len(reader.pages)
-        for i in range(0,number_of_pages):
-            page = reader.pages[i]
-            text = text + page.extract_text()
-
-        text = re.sub(r"[\S\s]*CONTA SIMPLES N.", "", text)
-        text = re.sub(r"SALDO FINAL[\S\s]*", "", text)
-
-        df_pdf = import_pdf(text)
+        pdf_content = extract_pdf_content(uploaded_file)
+        df_pdf = extract_content_df(pdf_content)
         df_trx = pd.concat([df_trx, df_pdf], ignore_index=True)
 
     df_trx = df_trx.merge(df_gs, how='left', on='description')
-    st.dataframe(df_trx, use_container_width=True)
+
+    # Show a multiselect widget with the genres using `st.multiselect`.
+    categories = st.multiselect(
+       "Categorias",
+       df_trx.category.unique(),
+       df_trx.category.unique(),
+    )
+
+    # Filter the dataframe based on the widget input and reshape it.
+    df_filtered = df_trx[(df_trx["category"].isin(categories))]
+
+    st.dataframe(df_filtered, use_container_width=True)
 
     # Grouping data
     df_chart = df_trx.groupby(['category', 'month'])['value'].sum().reset_index()
@@ -126,18 +139,12 @@ if uploaded_files:
     )
     st.altair_chart(chart, use_container_width=True)
 
-    st.write(df_chart)
+    #st.write(df_chart)
 
     #:chart_with_upwards_trend:
     #:money_with_wings:
     #:moneybag:
 
-# Show a multiselect widget with the genres using `st.multiselect`.
-#genres = st.multiselect(
-#    "Categorias",
-#    df_trx.categoria.unique(),
-#    ["Alimentação", "Transporte", "Lazer", "Arrendamento", "Saúde", "Educação"],
-#)
 #
 ## Show a slider widget with the years using `st.slider`.
 #years = st.slider("Years", 1986, 2006, (2000, 2016))
