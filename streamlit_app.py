b9eaacec92afb3f5634b82928b8b934ad9544bff
atch <alexandro.tommy@gmail.com>
2024-11-03 15:35:16 +0000

    adjust chart

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,10 @@
-import altair as alt
+import re, datetime
 import pandas as pd
+import altair as alt
 import streamlit as st
+from streamlit_gsheets import GSheetsConnection
 from io import BytesIO
 from pypdf import PdfReader
-import re
 
 
 # Show the page title and description.
@@ -16,6 +17,24 @@ st.write(
     Apenas faça o upload do seu extrato bancário e explore a aplicação!
     """
 )
+
+# Create a connection object.
+conn = st.connection("gsheets", type=GSheetsConnection)
+
+#df_gs = conn.read(
+#    worksheet="Depesas",
+#    ttl="10m",
+#    usecols=[4, 5],
+#    nrows=100,
+#)
+df_gs = conn.query("select distinct DESCRITIVO as description"                   
+                    ", Classificação as category, count(1) as c "
+                    " from Depesas group by 1,2 order by count(1) desc")
+
+df_gs['row_number'] = df_gs.groupby('description')['c'].rank(method='first', ascending=False).astype(int)
+df_gs['description'] = df_gs.description.apply(lambda a: re.sub(r"\s+", " ",re.sub(r"\d\d\d\d\s", "", a) ))
+df_gs['row_number'] = df_gs.groupby('description')['c'].rank(method='first', ascending=False).astype(int)
+df_gs = df_gs[df_gs.row_number==1][['description','category']]
 
 # Load the data from a CSV. We're caching this so it doesn't reload every time the app
 # reruns (e.g. if the user interacts with the widgets).
@@ -32,20 +51,30 @@ def import_pdf(text):
         data = re.sub(r" (?=\d+\.\d\d).*", "", row)
         valores = re.sub(r".*(?<!\d\d\.\d\d)\s", "", row)
         valor = valores.split(' ')[0]
+
         pattern = r"^\d+\.\d+ \d+\.\d+ (.*?) \d+\.\d+ \d+\.\d+$"
         match = re.search(pattern, row)
-        descricao=''
         if match:
             descricao = match.group(1)
+            descricao = re.sub(r"\d\d\d\d\s", "", descricao)
+            descricao = re.sub(r"\s+", " ", descricao)
+
         tipo_trans = descricao.split(' ')[0]
         descricao = ' '.join(descricao.split(' ')[1:])
 
-        trans_obj = {'data':data,
-                     'tipo_trans': tipo_trans,
-                     'descricao': descricao,
-                     'valor' : valor,
-                     'categoria' : ''}
-
+        try:
+            date_trx = datetime.date(2024, int(data.split('.')[0].strip()), int(data.split('.')[1]))
+        except:
+            pass
+        try:
+            trx_val = float(valor)
+        except:
+            pass
+        trans_obj = {'month': date_trx.month,
+                     'trx_type': tipo_trans,
+                     'description': descricao,
+                     'value': trx_val
+                     }
         transacoes.append(trans_obj)
         
     return pd.DataFrame.from_dict(transacoes)
@@ -60,7 +89,7 @@ df_trx = pd.DataFrame()
 if uploaded_files:
     for uploaded_file in uploaded_files:
         #bytes_data = uploaded_file.read()
-        st.write("filename:", uploaded_file.name)
+        print("filename:" + uploaded_file.name)
 
         # To convert to a string based IO:
         pdfio = BytesIO(uploaded_file.getvalue())
@@ -77,26 +106,31 @@ if uploaded_files:
         df_pdf = import_pdf(text)
         df_trx = pd.concat([df_trx, df_pdf], ignore_index=True)
 
-    st.write(df_trx)
+    df_trx = df_trx.merge(df_gs, how='left', on='description')
+    st.dataframe(df_trx, use_container_width=True)
+
+    # Grouping data
+    df_chart = df_trx.groupby(['category', 'month'])['value'].sum().reset_index()
 
     # Display the data as an Altair chart using `st.altair_chart`.
-    df_chart = pd.melt(
-        df_trx.reset_index(), id_vars="data", var_name="categoria", value_name="valor"
-    )
     chart = (
         alt.Chart(df_chart)
         .mark_line()
         .encode(
-            x=alt.X("Mes:N", title="Mês"),
-            y=alt.Y("Montante:Q", title="Montante ($)"),
-            color="Categoria:N",
+            x=alt.X("month:O", title="Mês"),  # Change to ordinal
+            y=alt.Y("value:Q", title="Despesas"),  # Change to quantitative
+            color=alt.Color("category:N", title="Categoria", scale=alt.Scale(scheme='category10')),
+            #strokeDash=alt.StrokeDash("category:N")  # Optional: Different dash styles for categories
         )
         .properties(height=320)
     )
     st.altair_chart(chart, use_container_width=True)
 
+    st.write(df_chart)
 
-
+    #:chart_with_upwards_trend:
+    #:money_with_wings:
+    #:moneybag:
 
 # Show a multiselect widget with the genres using `st.multiselect`.
 #genres = st.multiselect(
